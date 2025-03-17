f4dc147192d7b284d7ad3c2e6747108078929c75
Sarvnaz Alemohammad <55462526+sarvnazale@users.noreply.github.com>
2025-03-16 21:17:53 -0400

    pep8 formatting

--- data/2_fetch_data.py
+++ data/2_fetch_data.py
@@ -1,20 +1,22 @@
 import os
 
 # Define the command
-wget_command = """#!/bin/bash
+WGET_COMMAND = """#!/bin/bash
 wget -nv -r -nH --cut-dirs=7 \\
      -i speclist.txt \\
      -B https://data.sdss.org/sas/dr17/eboss/spectro/redux/v5_13_2/spectra/full/ \\
      -P data/
 """
 
-script_filename = "fetch_sdss_data.sh"
-with open(script_filename, "w") as script_file:
-    script_file.write(wget_command)
+SCRIPT_FILENAME = "fetch_sdss_data.sh"
+
+# Write the command to a script file
+with open(SCRIPT_FILENAME, "w") as script_file:
+    script_file.write(WGET_COMMAND)
 
 # Make the script executable
-os.chmod(script_filename, 0o755)
-print(f"Script '{script_filename}' has been created and made executable.")
+os.chmod(SCRIPT_FILENAME, 0o755)
+print(f"Script '{SCRIPT_FILENAME}' has been created and made executable.")
 
 # Uncomment the line below if you want to execute the script automatically
-os.system(f"./{script_filename}")
+# os.system(f"./{SCRIPT_FILENAME}")
--- data/4_preprocess.py
+++ data/4_preprocess.py
@@ -12,36 +12,74 @@ import warnings
 warnings.simplefilter('ignore', MergeConflictWarning)
 
 # List all FITS files
-# Get the repo root (assumes script is inside STARDUSTAI/)
+# Get the repository root (assumes script is inside STARDUSTAI/)
 repo_root = subprocess.check_output(["git", "rev-parse", "--show-toplevel"], text=True).strip()
 base_dir = os.path.join(repo_root, "data/full")
 file_paths = glob.glob(os.path.join(base_dir, "*/*.fits"))
 
-# If no FITS files are found, raise an error
+# Raise an error if no FITS files are found
 if not file_paths:
     raise ValueError("No FITS files found in 'data/full/'")
 
 
 def process_valid_fits(hdul):
+    """
+    Process valid FITS data into a pandas DataFrame.
+
+    This function extracts and processes data from the second and third HDUs
+    in a FITS file, reshaping and padding it as necessary.
+
+    Parameters
+    ----------
+    hdul : astropy.io.fits.HDUList
+        Opened FITS file HDU list.
+
+    Returns
+    -------
+    pd.DataFrame
+        Processed DataFrame with extracted and padded FITS data.
+    """
     dat1 = hdul[1].data
-    dat1 = np.array([dat1['flux'], dat1['loglam'], dat1['ivar'], dat1['model']]).T
+    dat1 = np.array(
+        [dat1['flux'], dat1['loglam'], dat1['ivar'], dat1['model']]
+    ).T    
     dat2 = hdul[2].data
-    sn_median_values = np.vstack(dat2['SN_MEDIAN'])  # Shape: (4590, 5)
-    sn_median_values = np.pad(sn_median_values, ((0, dat1.shape[0]-1), (0, 0)), 'constant', constant_values=0)
-    
-    dat2 = np.array([dat2['CLASS'], dat2['SUBCLASS'], dat2['PLATESN2'],  
-                 dat2['Z'], dat2['Z_ERR'], 
-                 dat2['ZWARNING'], dat2['RCHI2'], dat2['PLATEQUALITY']]).T
-    dat2 = np.pad(dat2, ((0, dat1.shape[0]-1), (0, 0)), 'constant', constant_values=0)
-    data = np.hstack([dat1, dat2])  # Merge HDUs
-    df = pd.DataFrame(data, columns=['flux', 'loglam', 'ivar', 'model', 'CLASS', 'SUBCLASS', 'PLATESN2', 'Z', 'Z_ERR', 'ZWARNING', 'RCHI2', 'PLATEQUALITY'])
+    sn_median_values = np.vstack(dat2['SN_MEDIAN']) 
+    sn_median_values = np.pad(
+        sn_median_values,
+        ((0, dat1.shape[0] - 1), (0, 0)),
+        'constant',
+        constant_values=0
+    )    
+    dat2 = np.array(
+        [
+            dat2['CLASS'], dat2['SUBCLASS'], dat2['PLATESN2'],
+            dat2['Z'], dat2['Z_ERR'],
+            dat2['ZWARNING'], dat2['RCHI2'], dat2['PLATEQUALITY']
+        ]
+    ).T
+    dat2 = np.pad(
+        dat2,
+        ((0, dat1.shape[0] - 1), (0, 0)),
+        'constant',
+        constant_values=0
+    )
+    # Merge HDUs
+    data = np.hstack([dat1, dat2])  
+    df = pd.DataFrame(
+        data,
+        columns=[
+            'flux', 'loglam', 'ivar', 'model', 'CLASS', 'SUBCLASS',
+            'PLATESN2', 'Z', 'Z_ERR', 'ZWARNING', 'RCHI2', 'PLATEQUALITY'
+        ]
+    )
     df['SN_MEDIAN_UV'] = sn_median_values[:, 0]
     df['SN_MEDIAN_R'] = sn_median_values[:, 2]
     df['SN_MEDIAN_NIR'] = sn_median_values[:, 3]
     df['SN_MEDIAN_IR'] = sn_median_values[:, 4]
     return df
 
-# go through filepaths, open fits files, save the fits file with zwarning = 0 or 16 and save it in a new folder as a pkl file 
+# Process FITS files and save data with ZWARNING = 0 or 16 as a .pkl file
 for file_path in tqdm(file_paths, mininterval=3):
     with fits.open(file_path) as hdul:
         dat2 = hdul[2].data
